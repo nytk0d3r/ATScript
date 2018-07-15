82acbbd72b1458f52f6e10131710ab9d52ce5712
nytk0d3r <nightcoder02@gmail.com>
2018-07-15 16:11:09 +0800

    Save

---
+++ ATScript.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+clear
+echo "~nytk0d3r"       
+echo "Original Script by Sync"                                                                                               
+
+
+HOST=""
+SERVER_PASSWORD=""
+USER=""
+HUB=""
+SE_PASSWORD=""
+ 
+HOST=${HOST}
+HUB=${HUB}
+USER_PASSWORD=${SERVER_PASSWORD}
+SE_PASSWORD=${SE_PASSWORD}
+ 
+echo -n "Server IP: "
+read HOST
+echo -n "Virtual Hub: "
+read HUB
+echo -n "${HUB} hub username: "
+read USER
+read -s -p "${HUB} hub password: " SERVER_PASSWORD
+echo ""
+read -s -p "SE Server password: " SE_PASSWORD
+echo ""
+echo " "
+echo "Wait until auto installation is finished."
+echo " "
+ 
+sudo apt-get -y update && sudo apt-get -y upgrade && apt-get install expect -y
+sudo apt-get install checkinstall build-essential -y
+wget http://www.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+tar -xzf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+rm -rf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+cd /root/vpnserver && expect -c 'spawn make; expect number:; send 1\r; expect number:; send 1\r; expect number:; send 1\r; interact'
+cd && mv vpnserver/ /usr/local && chmod 600 * /usr/local/vpnserver/ && chmod 700 /usr/local/vpnserver/vpncmd && chmod 700 /usr/local/vpnserver/vpnserver
+echo '#!/bin/sh
+# description: SoftEther VPN Server
+### BEGIN INIT INFO
+# Provides:          vpnserver
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: softether vpnserver
+# Description:       softether vpnserver daemon
+### END INIT INFO
+DAEMON=/usr/local/vpnserver/vpnserver
+LOCK=/var/lock/subsys/vpnserver
+test -x $DAEMON || exit 0
+case "$1" in
+start)
+$DAEMON start
+touch $LOCK
+;;
+stop)
+$DAEMON stop
+rm $LOCK
+;;
+restart)
+$DAEMON stop
+sleep 3
+$DAEMON start
+;;
+*)
+echo "Usage: $0 {start|stop|restart}"
+exit 1
+esac
+exit 0' > /etc/init.d/vpnserver
+###
+chmod 755 /etc/init.d/vpnserver && /etc/init.d/vpnserver start
+update-rc.d vpnserver defaults
+###
+echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
+sysctl -w net.ipv4.ip_forward=1
+sysctl --system
+echo "nameserver 1.1.1.1" > "/etc/resolv.conf"
+echo "nameserver 1.0.0.1" >> "/etc/resolv.conf"
+
+### Anti Torrenting for stuborn people ###
+iptables -A OUTPUT -p icmp --icmp-type echo-request -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+iptables -A INPUT -f -j DROP
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+iptables -A INPUT -m string --string "BitTorrent" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "BitTorrent protocol" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "peer_id=" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string ".torrent" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "announce.php?passkey=" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "torrent" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "announce" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "info_hash" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "peer_id" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "BitTorrent" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "BitTorrent protocol" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "bittorrent-announce" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "announce.php?passkey=" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "find_node" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "info_hash" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "get_peers" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "announce" --algo kmp --to 65535 -j DROP
+iptables -A INPUT -m string --string "announce_peers" --algo kmp --to 65535 -j DROP
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I OUTPUT -p tcp --dport 1723 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 6881:6889 -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -p tcp --dport 6881:6889 -j DROP
+iptables -D FORWARD -m string --algo bm --string "BitTorrent" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "BitTorrent protocol" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "peer_id=" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string ".torrent" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "announce.php?passkey=" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "torrent" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "announce" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "info_hash" -j LOGDROP
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j LOGDROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j LOGDROP
+iptables -A FORWARD -p udp -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "torrent" -j DROP 
+iptables -A FORWARD -p udp -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "info_hash" -j DROP 
+iptables -A FORWARD -p udp -m string --algo bm --string "tracker" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP iptables -A INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string ".torrent" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP iptables -A INPUT -p udp -m string --algo bm --string "torrent" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "announce" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "info_hash" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "tracker" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP iptables -I INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string ".torrent" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP iptables -I INPUT -p udp -m string --algo bm --string "torrent" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "announce" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "info_hash" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "tracker" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP iptables -D INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string ".torrent" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP iptables -D INPUT -p udp -m string --algo bm --string "torrent" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "announce" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "info_hash" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "tracker" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP iptables -I OUTPUT -p udp -m string --algo bm --string "peer_id=" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string ".torrent" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP iptables -I OUTPUT -p udp -m string --algo bm --string "torrent" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "announce" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "info_hash" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "tracker" -j DROP
+iptables -D INPUT -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D INPUT -m string --algo bm --string "BitTorrent protocol" -j DROP 
+iptables -D INPUT -m string --algo bm --string "peer_id=" -j DROP
+iptables -D INPUT -m string --algo bm --string ".torrent" -j DROP 
+iptables -D INPUT -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -D INPUT -m string --algo bm --string "torrent" -j DROP 
+iptables -D INPUT -m string --algo bm --string "announce" -j DROP
+iptables -D INPUT -m string --algo bm --string "info_hash" -j DROP
+iptables -D INPUT -m string --algo bm --string "tracker" -j DROP 
+iptables -D OUTPUT -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "peer_id=" -j DROP
+iptables -D OUTPUT -m string --algo bm --string ".torrent" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -D OUTPUT -m string --algo bm --string "torrent" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "announce" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "info_hash" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "tracker" -j DROP 
+iptables -D FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -D FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -D FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -D FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -D FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -D FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -D FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables -D FORWARD -m string --algo bm --string "tracker" -j DROP
+iptables-save
+ 
+
+iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP  
+
+iptables -N port-scanning 
+iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN 
+iptables -A port-scanning -j DROP
+ 
+HOST=${HOST}
+HUB_PASSWORD=${SE_PASSWORD}
+USER_PASSWORD=${SERVER_PASSWORD}
+ 
+TARGET="/usr/local/"
+ 
+${TARGET}vpnserver/vpncmd localhost /SERVER /CMD ServerPasswordSet ${SE_PASSWORD}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD HubCreate ${HUB} /PASSWORD:${HUB_PASSWORD}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD BridgeCreate /DEVICE:"soft" /TAP:yes ${HUB}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /HUB:${HUB} /CMD UserCreate ${USER} /GROUP:none /REALNAME:none /NOTE:none
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /HUB:${HUB} /CMD UserPasswordSet ${USER} /PASSWORD:${USER_PASSWORD}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD IPsecEnable /L2TP:yes /L2TPRAW:yes /ETHERIP:no /PSK:vpn /DEFAULTHUB:${HUB}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD HubDelete DEFAULT
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /HUB:${HUB} /CMD SecureNatEnable
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD VpnOverIcmpDnsEnable /ICMP:yes /DNS:yes
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 53
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 137
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 500
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 921
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 4500
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 4000
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 40000
+clear
+echo "Server configuration successful!"
+echo " "
+echo "Host: ${HOST}"
+echo "Virtual Hub: ${HUB}"
+echo "Port: 443, 53, 137"
+echo "Username: ${USER}"
+echo "Password: ${SERVER_PASSWORD}"
+echo "Server Password: ${SE_PASSWORD}"
+echo " "
